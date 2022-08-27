6465db7f81df95751425bc4cae522937cba17526
Chahid Adnane <adnanechahid@gmail.com>
2022-08-27 23:35:56 +0200

    feat(): implement lambda funtion using Runnable and BiFunction functional interfaces

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,17 @@
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class Test {
     public static void main(String[] args) {
          printName.accept("Adnane");
+         lineSeparator.run();
+        System.out.println(addNumbers.apply(11, 11));
+        lineSeparator.run();
     }
 
-    public static Consumer<String> printName = (name) -> System.out.println(name);
+    public static Consumer<String> printName = (name) -> System.out.println("My name is " + name);
+
+    public static Runnable lineSeparator = () -> System.out.println("--------------------------");
+
+    public static BiFunction<Integer, Integer, Integer> addNumbers = (x1, x2) -> x1 + x2;
 }
